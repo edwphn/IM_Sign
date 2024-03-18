3cf8d4f1a75884ee9cf2667e71a35ef196284ed4
ed <hello@pishon.store>
2024-03-18 22:09:30 +0100

    loguru

--- main.py
+++ main.py
@@ -2,39 +2,62 @@ from fastapi import FastAPI, Header, Request, HTTPException
 import uuid
 from datetime import datetime, timezone
 from database_handler import create_tables, execute_sql, insert_Sign_PDF, insert_Sign_PDF_history
-from validators import validate_file
+from validators import validate_file, sanitize_input
 import aiofiles
+from log_sett import setup_logger
+from loguru import logger
+
+setup_logger()
 
 app = FastAPI()
 
 app.add_event_handler("startup", create_tables)
 
-
 @app.post("/sign")
 async def sign(request: Request, sender: str = Header(...)):
+    sender = sanitize_input(sender)
     file_uuid = str(uuid.uuid4())
-    print(file_uuid)
+    logger.info(f"Generated UUID for file: {file_uuid} from sender: {sender}")
 
     content = await request.body()
 
     # Validate file
     is_valid = await validate_file(content)
     if not is_valid:
+        logger.warning(f"File validation failed for UUID: {file_uuid}")
         raise HTTPException(status_code=400, detail="Invalid file. Check the file integrity or size limit.")
 
     file_path = f"files/{file_uuid}.pdf"
-    async with aiofiles.open(file_path, 'wb') as out_file:
-        await out_file.write(content)
+    try:
+        async with aiofiles.open(file_path, 'wb') as out_file:
+            await out_file.write(content)
+        logger.info(f"File saved successfully at {file_path}")
+    except IOError as e:
+        logger.error(f"Failed to save file for UUID: {file_uuid}. Error: {e}")
+        raise HTTPException(status_code=500, detail="Failed to save file.")
 
     now = datetime.now(timezone.utc)
     file_size = len(content)
 
-    await execute_sql(insert_Sign_PDF, (file_uuid, now, 'unknown.pdf', None, file_size, now, sender))
-    await execute_sql(insert_Sign_PDF_history, (file_uuid, 'Received', 'Received file from the client', now))
+    try:
+        await execute_sql(insert_Sign_PDF, (file_uuid, now, 'unknown.pdf', None, file_size, now, sender))
+        await execute_sql(insert_Sign_PDF_history, (file_uuid, 'Received', 'Received file from client', now))
+        logger.info(f"Database operations successful for UUID: {file_uuid}")
+    except Exception as e:
+        logger.error(f"Database operation failed for UUID: {file_uuid}. Error: {e}")
+        raise HTTPException(status_code=500, detail="Database operation failed.")
 
     return {"uuid": file_uuid}
 
-
 if __name__ == "__main__":
+    logger.info("Starting FastAPI server on 127.0.0.1:8000")
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+'''
+подключить логгер
+загрузить сертификат
+включить подписывание 
+'''
+
