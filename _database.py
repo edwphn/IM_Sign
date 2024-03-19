1e4f31dc21dba474a1c1e78090acd8025610d686
ed <hello@pishon.store>
2024-03-19 21:34:35 +0100

    фваа

--- _database.py
+++ _database.py
@@ -48,3 +48,33 @@ async def execute_sql(sql, params=None):
 async def create_tables():
     await execute_sql_from_file('create_Documents.sql')
     await execute_sql_from_file('create_DocumentsHistory.sql')
+
+
+import pyodbc
+
+def execute_sql_sync(sql, params=None):
+    try:
+        connection = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};'
+            f'SERVER={config_vars["DATABASE"]["SERVER"]};'
+            f'DATABASE={config_vars["DATABASE"]["NAME"]};'
+            f'UID={config_vars["DATABASE"]["USER"]};'
+            f'PWD={config_vars["DATABASE"]["PASSWORD"]}',
+            autocommit=True
+        )
+
+    except pyodbc.Error as e:
+        print(f"Error connecting to the database: {e}")
+        raise
+
+    try:
+        cursor = connection.cursor()
+        if params is not None:
+            cursor.execute(sql, params)
+        else:
+            cursor.execute(sql)
+
+    finally:
+        cursor.close()
+        connection.close()
+
